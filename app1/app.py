d8101d6a4b5ce52cf279d63ec1411beed2d8b11d
Francis <68894029+frvfrvr@users.noreply.github.com>
2024-09-17 12:05:15 +0800

    day 1.5

--- app1/app.py
+++ app1/app.py
@@ -4,7 +4,7 @@ todo1 = input(user_prompt)
 todo2 = input(user_prompt)
 todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3] # List
+todos = [todo1, todo2, todo3, "Hello"] # List
 print(todos)
 
 print(type(todo1))
