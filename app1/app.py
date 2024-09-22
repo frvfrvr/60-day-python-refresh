c3f14f35a0cf5b4af32516fc52c1781c3abbd96a
Francis <68894029+frvfrvr@users.noreply.github.com>
2024-09-22 19:35:10 +0800

    Day 6: additional info

--- app1/app.py
+++ app1/app.py
@@ -18,6 +18,9 @@ while True:
             file.writelines(todos)
             file.close()
         case 'show':
+            file = open('./todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             for index, item in enumerate(todos):
                 print(f"{index + 1}. {item}")
         case 'edit':
--- app1/day6.md
+++ app1/day6.md
@@ -3,3 +3,4 @@
 - Storing items in text files
 - - value of variables are only stored temporarily in memory, that's when text files come in handy, or files a program can store its data on.
 - Reading and writing data in text files using `file.open()`, `file.write()`, `file.writelines()`
+- Data types
