930971ad175251f514f1afab82a622f76f45694f
Francis <68894029+frvfrvr@users.noreply.github.com>
2024-09-22 19:57:35 +0800

    Day 6: bonus

---
+++ app1/bonus6.py
@@ -0,0 +1,11 @@
+contents = ['quick brown fox jump over lazy dog', 
+            'lorem ipsum si vous plait', 
+            'yoroshiku onegaishimasu']
+
+filenames = ["doc.txt", 
+             "report.txt", 
+             "presentation.txt"]
+
+for content, filename in zip(contents, filenames):
+    file = open(f"./files/{filename}")
+    file.write(content)
--- app1/day6.md
+++ app1/day6.md
@@ -4,3 +4,4 @@
 - - value of variables are only stored temporarily in memory, that's when text files come in handy, or files a program can store its data on.
 - Reading and writing data in text files using `file.open()`, `file.write()`, `file.writelines()`
 - Data types
+- zip()
